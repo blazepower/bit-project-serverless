6e365c633395e786fcab42b1d7c999e82438d027
Rishik Hombal <hombalrishik@gmail.com>
2021-02-02 18:59:44 -0500

    added trigger files

--- vanillaJSClock/app.js
+++ vanillaJSClock/app.js
@@ -1,6 +1,8 @@
-var startTime = function () {
-    let time = new Date();
-    clock.textContent = time.toTimeString();
-}
-startTime();
-setInterval(startTime, 100);
+// var startTime = function () {
+//     let time = new Date();
+//     clock.textContent = time.toTimeString();
+// }
+// startTime();
+// setInterval(startTime, 100);
+
+console.log("Hello World");
